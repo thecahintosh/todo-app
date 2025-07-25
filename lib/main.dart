72fe3c83ee9e401f77685f9640dc49e2b6e06088
the_cahintosh <90477427+thecahintosh@users.noreply.github.com>
2025-07-25 23:03:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,16 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  @override
+ // @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+     // debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+     // home: HomePage(),
     );
   }
 }
